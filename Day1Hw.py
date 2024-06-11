16283c1a8a4b2ae804549187829a8dcabf54f361
Jeremiah  Williams <jae4lani17@gmail.com>
2024-06-10 23:15:18 -0400

    Add files via upload

---
+++ Day1Hw.py
@@ -0,0 +1,46 @@
+#  if statement Code correction
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("wear sunglasses!")
+
+else : weather != "sunny"
+print("Take an umbrella!")
+
+# your mood today
+input = "happy"
+
+if input == "happy":
+    print("That's great to hear!")
+
+else: input == "sad"
+print("i hope your day gets better!")
+
+#python data types and type() function
+#code correction
+variable_a = "Hello, World!" 
+variable_b = "23"
+variable_c = "3.14"
+variable_d = True
+
+type (variable_a)
+type (variable_b)
+type (variable_c)
+type (variable_d)
+print(variable_a)
+print(variable_b)
+print(variable_c)
+print(variable_d)
+
+
+#Arithmetic Operations in Daily Life
+#Grocery store math
+
+bread = 2.75
+peanutbutter = 4.50
+jelly = 3.23
+
+print(bread + peanutbutter + jelly)
+
+
